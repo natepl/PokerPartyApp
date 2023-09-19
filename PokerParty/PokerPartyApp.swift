9bfd4d88efbf7a5375ee4a6a1b0f7ad4c5346735
natepl <68605773+natepl@users.noreply.github.com>
2023-09-18 23:21:26 -0400

    Initial Commit

---
+++ PokerParty/PokerPartyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokerPartyApp.swift
+//  PokerParty
+//
+//  Created by Nathaniel Parry Luff on 9/18/23.
+//
+
+import SwiftUI
+
+@main
+struct PokerPartyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
